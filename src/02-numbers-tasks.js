f1e6d78be9aa18a637fd0347188f182ebfc396ee
Natalia Furmanova <nfurmanova@tula.co>
2024-10-09 23:44:40 +0300

    getAngleBetweenVectors

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -110,8 +110,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return (((x1 * x2 + y1 * y2)
-  / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))) * Math.PI) / 180;
+  return Math.acos(((x1 * x2 + y1 * y2)
+  / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))));
 }
 
 /**
