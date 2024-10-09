90060fac99a4b3c03be01f1a4bb627c1287dba7c
Natalia Furmanova <nfurmanova@tula.co>
2024-10-09 23:53:34 +0300

    getRectangleString

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -203,7 +203,9 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-  return width * height;
+  return `┌${'─'.repeat(width - 2)}┐\n${
+    `│${' '.repeat(width - 2)}│\n`.repeat(height - 2)
+  }└${'─'.repeat(width - 2)}┘\n`;
 }
 
 
